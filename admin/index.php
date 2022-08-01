7d908da77873aef51cf59791e1defc4090fbccfa
DR4CH3N <thehellc4t@gmail.com>
2022-08-01 10:53:17 -0300

    meu perfil pronta

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,32 @@
-<?php 
-require_once "../inc/cabecalho-admin.php";
-?>
+<?php
 
+use Microblog\Usuario;
+
+require_once "../inc/cabecalho-admin.php";
+
+$usuario = new Usuario;
+
+$usuario->setId($_SESSION['id']);
+$dados = $usuario->listarUm();
+
+
+if (isset($_GET['perfil_atualizado']) ) {
+	$feedback = "Dados atualizados com sucesso!";
+}
+
+?>
 
 <article class="p-5 my-4 rounded-3 bg-white shadow">
     <div class="container-fluid py-1">        
-        <h2 class="display-4">Olá <?=$_SESSION['nome']?></h2>
+        <h2 class="display-4">Olá <?=$dados['nome']?></h2>
+        
+        <?php if(isset($feedback)){?>
+				<p class="my-2 alert alert-primary text-center">
+					<?=$feedback?>
+				</p>
+                <?php } ?>
+
+
         <p class="fs-5">Você está no <b>painel de controle e administração</b> do
 		site Microblog e seu <b>nível de acesso</b> é <span class="badge bg-dark"> <?=$_SESSION['tipo']?></span></p>
         <hr class="my-4">
--- admin/meu-perfil.php
+++ admin/meu-perfil.php
@@ -1,5 +1,50 @@
 <?php 
 require_once "../inc/cabecalho-admin.php";
+
+use Microblog\Usuario;
+use Microblog\Utilitarios;
+
+
+
+$usuario = new Usuario;
+
+$usuario->setId($_SESSION['id']);
+$dados = $usuario->listarUm();
+
+
+
+
+// Utilitarios::dump($dados);
+
+if (isset($_POST['atualizar'])) {
+	$usuario->setNome($_POST['nome']);
+	$usuario->setEmail($_POST['email']);
+	$usuario->setTipo($_SESSION['tipo']);
+
+	// LOGICA DA SENHA:
+
+	/* algoritmo da senha
+	se o campo senha do furmulario no formulario estiver vazio, 
+	signfica que o usuario NÃO MUDOU A SENHA.
+	*/
+	if (empty($_POST['senha']) ) {
+		$usuario->setSenha( $dados['senha'] );
+		echo $usuario->getSenha();
+	}
+	else {
+		/* Caso contrario, se o usuario digitou alguma coisa no campo senha, precisaremos verificar o que foi digitado 
+		*/
+
+		$usuario->setSenha(  
+			$usuario->verificaSenha($_POST['senha'], $dados['senha'])
+		);
+
+		// echo $usuario->verificaSenha($_POST['senha'], $dados['senha']);
+	}
+
+	$usuario->atualizar();
+	header("location:index.php?perfil_atualizado");
+}
 ?>
 
 
@@ -10,21 +55,22 @@ require_once "../inc/cabecalho-admin.php";
 		Atualizar meus dados
 		</h2>
 				
-		<form class="mx-auto w-75" action="" method="post" id="form-atualizar" name="form-atualizar">
+		<form class="mx-auto w-75" action="" method="post" id="form-atualizar">
+
 
 			<div class="mb-3">
 				<label class="form-label" for="nome">Nome:</label>
-				<input class="form-control" type="text" id="nome" name="nome" required>
+				<input value="<?=$dados['nome']?>" class="form-control" type="text" id="nome" name="nome" required>
 			</div>
 
 			<div class="mb-3">
 				<label class="form-label" for="email">E-mail:</label>
-				<input class="form-control" type="email" id="email" name="email" required>
+				<input value="<?=$dados['email']?>" class="form-control" type="email" id="email" name="email" required>
 			</div>
 
 			<div class="mb-3">
 				<label class="form-label" for="senha">Senha:</label>
-				<input class="form-control" type="password" id="senha" name="senha" placeholder="Preencha apenas se for alterar">
+				<input  class="form-control" type="password" id="senha" name="senha" placeholder="Preencha apenas se for alterar">
 			</div>
 
 			<button class="btn btn-primary" name="atualizar"><i class="bi bi-arrow-clockwise"></i> Atualizar</button>
